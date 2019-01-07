836ed3990212c658a20dc3bde0aca78207dbe113
E.K. KIM <ekkim@netvisiontel.com>
2019-01-08 08:05:21 +0900

    version from __init__.py

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+from backupman import VERSION
+
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
@@ -40,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.0',  # Required
+    version=VERSION,  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
